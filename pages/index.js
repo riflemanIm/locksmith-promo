43dc8ef7f4fda7abf1b96be4f7dd6c28467153d1
Oleg La <oleglambin@gmail.com>
2025-07-13 15:31:37 +0300

    feat: favicon

--- pages/index.js
+++ pages/index.js
@@ -15,9 +15,25 @@ export default function Home() {
     <>
       <Head>
         <title>{title}</title>
+
         <meta name="description" content={description} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="canonical" href={siteUrl} />
+        {/* Favicon и иконки */}
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon-96x96.png"
+          sizes="96x96"
+        />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
 
         {/* Open Graph */}
         <meta property="og:type" content="website" />
