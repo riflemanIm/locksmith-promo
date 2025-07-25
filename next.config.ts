51510877018a92871fd5452b4dedfb9b1bd8f7ff
Oleg La <oleglambin@gmail.com>
2025-07-25 12:41:29 +0300

    feat:  export

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // Отключаем SSR: экспортируем только статику
+  output: "export",
   trailingSlash: false,
 };
 
